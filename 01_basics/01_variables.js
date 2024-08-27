f8ad3529da285e48721602088defe2626bdb54bf
Rananjay09 <rananjay.singh.909@gmail.com>
2024-08-27 15:16:50 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId= 10000
+let accountName="rjSinghh"
+var accountEmaiId="rj@gamil.com"
+accountCity="Bengaluru"
+
+console.log(accountId);
+
+//accountId=2000 won't make any changes becoz constant
+
+accountCity="Mumbai"
+console.table([accountId, accountName, accountEmaiId, accountCity]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; //treat all JS code as newer version
+
+//alert( 3 + 3)//we are not using nodejs, web browser, this happens there
+ 
+console.log(3
+    
+    +3)//wrong practice, code should be readable
+
+console.log("rj")
+
+let number= 20
+let name= "rj"
+let isLoggedIn=false
+
+console.log(typeof null); //object
+console.log(typeof undefined)// undefined 
